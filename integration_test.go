6e827aa6c70e462b0576bb20b8e0f691923dbb9d
XrXr <xrsquared@gmail.com>
2018-09-09 14:37:27 -0400

    Add indirect sign extension test cases

--- integration_test.go
+++ integration_test.go
@@ -6,91 +6,167 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 	"os/exec"
 	"path"
 	"testing"
 )
 
-func TestIntegration(t *testing.T) {
+func TestHandMadeCases(t *testing.T) {
 	gopath := os.Getenv("GOPATH")
-	if len(gopath) == 0 {
-		t.Error("GOPATH environmental variable not set")
-		return
-	}
-	fmt.Println("running go install...")
-	cmd := exec.Command("go", "install")
-	err := cmd.Start()
-	if err != nil {
-		t.Error("Failed to start `go install`")
-		return
-	}
-	err = cmd.Wait()
-	if err != nil {
-		t.Error("`go install` failed to complete")
-		return
-	}
-
 	fixturePath := path.Join(gopath, "src/github.com/XrXr/alang/test")
 	files, err := ioutil.ReadDir(fixturePath)
 	if err != nil {
 		t.Error("Failed to list files in fixture directory")
 		return
 	}
-	compiledBinPath := path.Join(os.TempDir(), "alang_integration_bin")
 	for _, f := range files {
 		name := f.Name()
 		if path.Ext(name) != ".al" {
 			continue
 		}
 		t.Run(name, func(t *testing.T) {
-			inFixturePath := path.Join(fixturePath, name)
-			outFixturePath := inFixturePath + ".out"
+			sourcePath := path.Join(fixturePath, name)
+			outFixturePath := sourcePath + ".out"
 			expected, err := ioutil.ReadFile(outFixturePath)
 			if err != nil {
-				t.Error(err)
-				return
+				t.Fatal(err)
 			}
-			compilerCommand := exec.Command(path.Join(gopath, "bin", "alang"), "-o", compiledBinPath, inFixturePath)
-			err = compilerCommand.Start()
-			if err != nil {
-				t.Error("Failed to invoke compiler for")
-				return
-			}
-			err = compilerCommand.Wait()
-			if err != nil {
-				t.Error("Failed to compile")
-				return
-			}
-			compiled := exec.Command(compiledBinPath)
-			out, err := compiled.StdoutPipe()
-			if err != nil {
-				t.Error("Failed to create pipe to the compiled command", err)
-				return
-			}
-			err = compiled.Start()
-			if err != nil {
-				t.Error("Failed to start the compiled executable")
-				return
-			}
-			outBytes, err := ioutil.ReadAll(out)
-			if err != nil {
-				t.Error("Failed read from stdout of compiled binary")
-				return
-			}
-			err = compiled.Wait()
-			if err != nil {
-				t.Error("Compiled executable failed to finish")
-				return
-			}
-			if !bytes.Equal(outBytes, expected) {
-				t.Error("Output from compiled binary is incorrect")
-				return
-			}
-			err = os.Remove(compiledBinPath)
-			if err != nil {
-				t.Error("Failed to clean out compiled binary")
-			}
+			compileAndAssertOutput(t, sourcePath, expected)
 		})
 	}
+}
+
+var signedTypes = []string{"s8", "s16", "s32", "s64"}
+var unsignedTypes = []string{"u8", "u16", "u32", "u64"}
+
+func TestIndirectSignAndZeroExtensions(t *testing.T) {
+	for smallerSize := 8; smallerSize < 64; smallerSize *= 2 {
+		smallerSigned := fmt.Sprintf("s%d", smallerSize)
+		smallerUnsigned := fmt.Sprintf("u%d", smallerSize)
+		for biggerSize := smallerSize * 2; biggerSize <= 64; biggerSize *= 2 {
+			biggerSigned := fmt.Sprintf("s%d", biggerSize)
+			biggerUnsigned := fmt.Sprintf("u%d", biggerSize)
+			t.Run(fmt.Sprintf("Load *%s into %s", smallerSigned, biggerSigned), makeIndirectLoadFromSmall(smallerSigned, biggerSigned, -107))
+			t.Run(fmt.Sprintf("Load %s into *%s", smallerSigned, biggerSigned), makeIndirectWriteToBig(smallerSigned, biggerSigned, -98))
+
+			t.Run(fmt.Sprintf("Load *%s into %s", smallerUnsigned, biggerUnsigned), makeIndirectLoadFromSmall(smallerUnsigned, biggerUnsigned, 201))
+			t.Run(fmt.Sprintf("Load %s into *%s", smallerUnsigned, biggerUnsigned), makeIndirectWriteToBig(smallerUnsigned, biggerUnsigned, 201))
+		}
+	}
+}
+
+func makeIndirectLoadFromSmall(smallerType string, biggerType string, magicConstant int) func(*testing.T) {
+	const template = `
+	main :: proc () {
+		var a %s
+		var b %s
+		ap := &a
+		a = %[3]d
+		b = -1
+
+		b = @ap
+		if b == %[3]d {
+			puts("good\n")
+		}
+	}
+`
+
+	program := fmt.Sprintf(template, smallerType, biggerType, magicConstant)
+	return func(t *testing.T) {
+		testSourceString(t, program, []byte("good\n"))
+	}
+}
+
+func makeIndirectWriteToBig(smallerType string, biggerType string, magicConstant int) func(*testing.T) {
+	const template = `
+	main :: proc () {
+		var a %s
+		var b %s
+		bp := &b
+		a = %[3]d
+		b = -1
+
+		@bp = a
+		if b == %[3]d {
+			puts("good\n")
+		}
+	}
+`
+
+	program := fmt.Sprintf(template, smallerType, biggerType, magicConstant)
+	return func(t *testing.T) {
+		testSourceString(t, program, []byte("good\n"))
+	}
+}
+
+func testSourceString(t *testing.T, program string, expectedOutput []byte) {
+	sourcePath := path.Join(os.TempDir(), "alang_test_source")
+
+	sourceFile, err := os.OpenFile(sourcePath, os.O_RDWR|os.O_CREATE, 0600)
+	if err != nil {
+		t.Fatal("Failed to open source file for writing")
+	}
+	err = sourceFile.Truncate(0)
+	if err != nil {
+		t.Fatal("Failed to truncate the source file before writing")
+	}
+	_, err = sourceFile.WriteString(program)
+	if err != nil {
+		t.Fatal("Failed to write source code to file")
+	}
+	sourceFile.Close()
+	compileAndAssertOutput(t, sourcePath, expectedOutput)
+}
+
+func compileAndAssertOutput(t *testing.T, sourcePath string, expected []byte) {
+	gopath := os.Getenv("GOPATH")
+	binPath := path.Join(os.TempDir(), "alang_test_bin")
+	compilerCommand := exec.Command(path.Join(gopath, "bin", "alang"), "-o", binPath, sourcePath)
+	compilerOutput, err := compilerCommand.CombinedOutput()
+	if err != nil {
+		t.Fatalf("Failed to compile. Compiler output:\n%s\n", compilerOutput)
+	}
+	compiled := exec.Command(binPath)
+	outPipe, err := compiled.StdoutPipe()
+	if err != nil {
+		t.Fatal("Failed to create pipe to the compiled command", err)
+	}
+	err = compiled.Start()
+	if err != nil {
+		t.Fatal("Failed to start the compiled executable")
+	}
+	outBytes, err := ioutil.ReadAll(outPipe)
+	if err != nil {
+		t.Fatal("Failed to read from stdout of compiled binary")
+	}
+	err = compiled.Wait()
+	if err != nil {
+		t.Fatal("Compiled executable failed to finish")
+	}
+	defer os.Remove(binPath)
+
+	if !bytes.Equal(outBytes, expected) {
+		t.Fatal("Compiled binary's output differs from expectation")
+	}
+}
+
+func TestMain(m *testing.M) {
+	log.SetFlags(0)
+	gopath := os.Getenv("GOPATH")
+	if len(gopath) == 0 {
+		log.Fatalln("GOPATH environmental variable not set")
+	}
+	log.Println("Running go install on the compiler...")
+	cmd := exec.Command("go", "install", "github.com/XrXr/alang")
+	err := cmd.Start()
+	if err != nil {
+		log.Fatalln("Failed to start `go install`")
+	}
+	err = cmd.Wait()
+	if err != nil {
+		log.Fatalln("`go install` failed to complete")
+	}
+	os.Exit(m.Run())
 }
